2cd00925e070c700c6e0f5e73e9369633d09b919
Andy Birkett <abirkett13@gmail.com>
2016-04-14 09:46:50 +0100

    [AB] Debug logging

--- src/main/java/uk/gov/digital/ho/proving/income/family/cwtool/ServiceRunner.java
+++ src/main/java/uk/gov/digital/ho/proving/income/family/cwtool/ServiceRunner.java
@@ -1,5 +1,7 @@
 package uk.gov.digital.ho.proving.income.family.cwtool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -8,12 +10,21 @@ import static org.springframework.boot.SpringApplication.run;
 @SpringBootApplication
 public class ServiceRunner  {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(ServiceRunner.class);
+
     //@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ServiceRunner.class);
     }
 
     public static void main(String[] args) throws Exception {
+
+        String remotePort = System.getProperty("remote.server.port", "8081");
+
+        String url = "http://localhost:"+ remotePort;
+
+        LOGGER.info("Remote url: " + url);
+
         run(ServiceRunner.class, args);
     }
 
