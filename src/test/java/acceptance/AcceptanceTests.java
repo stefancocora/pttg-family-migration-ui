55b07f8ba980e08155e54022006884776c5bd7ef
Mitchell Agoma <mitchell_agoma@yahoo.co.uk>
2016-04-27 13:56:39 +0100

    [MA] Updated feature files

--- src/test/java/acceptance/AcceptanceTests.java
+++ src/test/java/acceptance/AcceptanceTests.java
@@ -5,9 +5,9 @@ import net.serenitybdd.cucumber.*;
 import org.junit.runner.*;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features={"src/test/specs/income-proving/family-tm-caseworker-tool/design-v3"}
+@CucumberOptions(features={"src/test/specs/income-proving/family-tm-caseworker-tool/design-v4"}
         , glue={"steps"}
-        //,tags = {"@SD63, @SD41, @SD64, @SD65"}
+        ,tags = {"@SD102-Tool_identifies_Applicant_Does_Not_Neet_CAT_A_Dependent"}
 )
 
 public class AcceptanceTests {
