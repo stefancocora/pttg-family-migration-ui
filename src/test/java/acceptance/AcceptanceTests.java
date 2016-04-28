289a4f9a908296b378a8db30dee8a2f0d79b5aa9
Mitchell Agoma <mitchell_agoma@yahoo.co.uk>
2016-04-28 08:21:29 +0100

    [MA] Updated feature files

--- src/test/java/acceptance/AcceptanceTests.java
+++ src/test/java/acceptance/AcceptanceTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.*;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features={"src/test/specs/income-proving/family-tm-caseworker-tool/design-v4"}
         , glue={"steps"}
-        ,tags = {"@SD102-Tool_identifies_Applicant_Does_Not_Neet_CAT_A_Dependent"}
+        ,tags = {"@SD102-Tool_identifies_Applicant_Does_Not_Meet_CAT_A_Dependent"}
 )
 
 public class AcceptanceTests {
