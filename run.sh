3aa5a7e67278180ab1fe132ac5fafcb878409e56
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-09-02 15:47:16 +0100

    adding memory allocation to jar run command

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 NAME=${NAME:-pttg-income-proving-fm-ui}
 
 JAR=$(find . -name ${NAME}*.jar|head -1)
-java -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+java -Xmx750m -Xms256m -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
